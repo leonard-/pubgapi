b048d454fddbf46ce11679ba76da2e8c9a9cb44a
floholz <44244243+floholz@users.noreply.github.com>
2019-10-18 21:50:41 +0200

    Added missing maps

--- src/main/java/com/github/gplnature/pubgapi/model/Map.java
+++ src/main/java/com/github/gplnature/pubgapi/model/Map.java
@@ -3,7 +3,10 @@ package com.github.gplnature.pubgapi.model;
 import com.google.gson.annotations.SerializedName;
 
 public enum Map {
-    @SerializedName("Savage_Main") SAVAGE_MAIN,
     @SerializedName("Desert_Main") DESERT_MAIN,
-    @SerializedName("Baltic_Main") BALTIC_MAIN
+    @SerializedName("Erangel_Main") ERANGEL_MAIN,
+    @SerializedName("Savage_Main") SAVAGE_MAIN,
+    @SerializedName("Baltic_Main") BALTIC_MAIN,
+    @SerializedName("Range_Main") RANGE_MAIN,
+    @SerializedName("DihorOtok_Main") DIHOROTOK_MAIN
 }
