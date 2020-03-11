e4a51ec4000fdc511d84c147ab4fc1fa594d46ff
Christoph Thelen <commana@gmx.de>
2020-03-11 14:32:45 +0100

    Implement changes for Vending Machine usage

---
+++ src/main/java/com/github/gplnature/pubgapi/model/telemetry/object/ObjectAdditionalInfo.java
@@ -0,0 +1,22 @@
+package com.github.gplnature.pubgapi.model.telemetry.object;
+
+public class ObjectAdditionalInfo {
+    private String first;
+    private Integer second;
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public Integer getSecond() {
+        return second;
+    }
+
+    public void setSecond(Integer second) {
+        this.second = second;
+    }
+}
