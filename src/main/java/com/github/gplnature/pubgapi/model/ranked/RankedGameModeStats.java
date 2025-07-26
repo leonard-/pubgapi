40c401352aca58436f4584d406cde281d6114800
Leonard <4254155+leonard-@users.noreply.github.com>
2025-07-26 13:03:09 +0200

    add duo ranked stats

--- src/main/java/com/github/gplnature/pubgapi/model/ranked/RankedGameModeStats.java
+++ src/main/java/com/github/gplnature/pubgapi/model/ranked/RankedGameModeStats.java
@@ -3,11 +3,24 @@ package com.github.gplnature.pubgapi.model.ranked;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class RankedGameModeStats {
+    @JsonProperty("duo")
+    private RankedGameModeStatsObj duo;
+    @JsonProperty("duo-fpp")
+    private RankedGameModeStatsObj duofpp;
+    @JsonProperty("squad")
     private RankedGameModeStatsObj squad;
     @JsonProperty("squad-fpp")
     private RankedGameModeStatsObj squadfpp;
     @JsonProperty("All")
     private RankedGameModeStatsObj all;
+
+    public RankedGameModeStatsObj getDuo() {
+        return duo;
+    }
+
+    public RankedGameModeStatsObj getDuofpp() {
+        return duofpp;
+    }
 
     public RankedGameModeStatsObj getSquad() {
         return squad;
